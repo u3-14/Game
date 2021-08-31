0d394e9c35d88d34fb3d275a07364e5d095d1b58
iogh-in-keds <ivanushkaprog2018@mail.ru>
2021-08-31 16:41:10 +0300

    start create money

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyScript : MonoBehaviour
+{
+    public ulong _MoneyCheck = 0;
+    private Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        _MoneyCheck += 10000000000000000000;
+        _text.text = _MoneyCheck.ToString();
+    }
+}
