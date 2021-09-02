fc9a2e82f1f6cdb21fc556285f1f196348e2c5ac
iogh-in-keds <ivanushkaprog2018@mail.ru>
2021-09-02 22:00:10 +0300

    new buttons

---
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("PlayMenu");
+    }
+    public void Shop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
