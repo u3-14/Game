57fa5393b6073206dd54ab15d87523821ddf6f3a
iogh-in-keds <ivanushkaprog2018@mail.ru>
2021-09-09 21:46:42 +0300

    shop animate create

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -6,16 +6,14 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     private int money = 0, skinNumber = 0;
-    public GameObject[] skins;
-    private Animator[] goToAnim;
+    public GameObject[] scins;
+    public GameObject shop;
     private bool[] buyed;
+    private bool a = true;
 
     private void Start()
     {
-        for(int i = 0; i < skins.Length; i++)
-        {
-            goToAnim[i] = skins[i].GetComponent<Animator>();
-        }
+        
     }
 
     private void Update()
@@ -25,16 +23,64 @@ public class ShopManager : MonoBehaviour
 
     public void Left()
     {
-        
+        if (skinNumber > 0 && a)
+        {
+            skinNumber--;
+            StartCoroutine("ToLeftCorutine");
+        } 
     }
 
     public void Right()
     {
-        
+        if (skinNumber < scins.Length - 1 && a)
+        {
+            skinNumber++;
+            StartCoroutine("ToRightCorutine");
+        } 
     }
 
     public void Buy()
     {
         
+    }
+
+    IEnumerator ToRightCorutine()
+    {
+        a = false;
+        Vector3 go = new Vector3(shop.transform.position.x, shop.transform.position.y, shop.transform.position.z);
+        for (int i = 0; i < 40; i++)
+        {
+            go.x -= .75f;
+            shop.transform.position = go;
+            yield return null;
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            go.x += .25f;
+            shop.transform.position = go;
+            yield return null;
+        }
+
+        a = true;
+    }
+    
+    IEnumerator ToLeftCorutine()
+    {
+        a = false;
+        Vector3 go = new Vector3(shop.transform.position.x, shop.transform.position.y, shop.transform.position.z);
+        for (int i = 0; i < 40; i++)
+        {
+            go.x += .75f;
+            shop.transform.position = go;
+            yield return null;
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            go.x -= .25f;
+            shop.transform.position = go;
+            yield return null;
+        }
+
+        a = true;
     }
 }
